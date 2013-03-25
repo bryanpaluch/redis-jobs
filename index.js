eeb76369d6f7c5c0b6f4cf3e38caf3125757eb77
Bryan Paluch <bryanpaluch@gmail.com>
2013-03-25 13:22:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var Queue = require('./lib/Queue.js');
+var Worker = require('./lib/Worker.js');
+//var Monitor = require('./lib/Monitor.js');
+
+module.exports = {Queue : Queue,
+                  Worker: Worker};
