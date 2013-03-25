8d39d2f6e3f04684276feb5fcbe078d512a05ab4
Bryan Paluch <bryanpaluch@gmail.com>
2013-03-25 16:47:56 -0400

    update

--- test/0-index.js
+++ test/0-index.js
@@ -1,18 +1,63 @@
-var redisjobs = require('../index');
 var assert = require('assert');
+var mockery = require('mockery');
+var fakeredis = require('fakeredis');
+var redisjobs;
+var queue;
+var client = fakeredis.createClient();
 
 describe('MessageQueue', function(){
+  before(function(done){
+    mockery.enable();
+    mockery.registerMock('redis', fakeredis);
+    mockery.warnOnReplace(false);
+    mockery.warnOnUnregistered(false);
+    redisjobs = require('../index');
+    done();
+  });
   it('Constructor', function(done){
-    var queue = new redisjobs.Queue();
+    queue = new redisjobs.Queue();
     assert.ok(queue);
     done();
   });
   it('MessageQueue.createJob(details) creates a job and returns it', function(done){
-    var queue = new redisjobs.Queue();
     var jobData = { foo: 'bar'};
     var job = queue.createJob(jobData);
-    console.log(job);
     done();
+  });
+  it('Job.on(\'timeout\', function(){} will timeout after default 5000 ms', function(done){
+    this.timeout(6000); 
+    var jobData = { foo: 'bar'};
+    var job = queue.createJob(jobData);
+    job.on('timeout', function(){
+      done();
+    });
+    job.start();
+  });
+  it('test', function(done){
+    client.subscribe('blah', function(channel, msg){
+      console.log(msg);
+    });
+    var client2 = fakeredis.createClient();
+    client2.publish('blah', 'hi', function(){});
+
+  });
+  it('Job.on(\'reply\', function(){} will answer when the worker finishes a job', function(done){
+    var jobData = { foo: 'bar'};
+    var job = queue.createJob(jobData);
+    job.on('reply', function(res){
+      console.log(res);
+      done();
+    });
+    console.log(job.req);
+    var jobRes = { "reply"    : { foo: 'baz'},
+                   "id"       : job.req.id,
+                   "time"     : job.req.time,
+                   "requester": job.req.requester };
+    console.log(jobRes);
+    job.start();
+    console.log(queue.subscriber);
+    queue.subscriber.pushMessage(job.req.requester, JSON.stringify(jobRes), function(){});
+
   });
 });
 
